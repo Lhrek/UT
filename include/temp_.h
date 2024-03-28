1f48ffd38cb26ca1eec1852f9c71f2c3bc298907
Zhijie Gan <herujiege@gmail.com>
2024-03-28 14:17:35 +0800

    mac commit

--- include/temp_.h
+++ include/temp_.h
@@ -3,18 +3,36 @@
 
 #include "header.h"
 /*2024.3.26 lc215*/
-    int findKthLargest(vector<int>& nums, int k) {
-        std::sort(nums.begin(), nums.end(), std::greater<int>());
-        return nums[k-1];
+int findKthLargest(vector<int> &nums, int k) {
+  std::sort(nums.begin(), nums.end(), std::greater<int>());
+  return nums[k - 1];
+}
+
+/*2024.3.27 lc15*/
+vector<vector<int>> threeSum(vector<int> &nums) {
+  std::set<vector<int>> unique_triples;
+  for (int i = 0; i < nums.size() - 2; i++) {
+    for (int j = i + 1; j < nums.size() - 1; j++) {
+      for (int k = j + 1; k < nums.size(); k++) {
+        if (nums[i] + nums[j] + nums[k] == 0) {
+          vector<int> temp = {nums[i], nums[j], nums[k]};
+          std::sort(temp.begin(), temp.end());
+          unique_triples.insert(temp);
+        }
+      }
     }
+  }
+  vector<vector<int>> res(unique_triples.begin(), unique_triples.end());
+  return res;
+}
 
 
 
 
 
+/*2024.3.27 lc53*/
+    int maxSubArray(vector<int>& nums) {
 
-
-
-
+    }
 
 #endif
--- src/UTmain.cpp
+++ src/UTmain.cpp
@@ -1,10 +1,18 @@
 #include "/Users/hrek/SE/UT/include/array_.h"
 #include "/Users/hrek/SE/UT/include/header.h"
 #include "/Users/hrek/SE/UT/include/string_.h"
+#include "/Users/hrek/SE/UT/include/temp_.h"
 
 
 
 
 int main() {
   string S = "abcabcbb";
+  vector<int> s = {-1,0,1,2,-1,-4};
+    for (const auto &triple : threeSum(s)) {
+        for (int num : triple) {
+            std::cout << num << " ";
+        }
+        std::cout << std::endl;
+    }
 }
