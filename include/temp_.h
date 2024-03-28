9b533554689c08663cc4586556071397850e9571
Zhijie Gan <herujiege@gmail.com>
2024-03-28 17:45:18 +0800

    mac commit

--- include/temp_.h
+++ include/temp_.h
@@ -27,17 +27,31 @@ vector<vector<int>> threeSum(vector<int> &nums) {
 }
 
 /*2024.3.27 lc53*/
-//暴力 超时
+// 暴力 超时
+// int maxSubArray(vector<int> &nums) {
+//   int MaxSum = -INT_MAX;
+//   for (int i = 0; i < nums.size(); i++) {
+//     int temp_sum = 0;
+//     for (int j = i; j < nums.size(); j++) {
+//       temp_sum += nums[j];
+//       MaxSum = std::max(MaxSum, temp_sum);
+//     }
+//   }
+//   return MaxSum;
+// }
+
+// 动态规划
 int maxSubArray(vector<int> &nums) {
-    int MaxSum = -INT_MAX;
-    for (int i = 0; i < nums.size(); i++) {
-        int temp_sum = 0;
-        for (int j = i; j < nums.size(); j++) {
-            temp_sum += nums[j];
-            MaxSum = std::max(MaxSum, temp_sum);
-        }
+  vector<int> dp = {};
+  dp.push_back(nums[0]);
+  for (int i = 1; i < nums.size(); i++) {
+    if (dp[i - 1] <= 0) {
+      dp.push_back(nums[i]);
+    } else {
+      dp.push_back(dp[i - 1] + nums[i]);
     }
-    return MaxSum;
+  }
+  return *max_element(dp.begin(), dp.end());
 }
 
 #endif
