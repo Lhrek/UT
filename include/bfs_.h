99d22420446340d370c02b2b02f185b1626158bf
Zhijie Gan <herujiege@gmail.com>
2024-06-12 15:57:57 +0800

    mac commit

--- include/bfs_.h
+++ include/bfs_.h
@@ -2,9 +2,24 @@
 #define __BFS_H
 
 #include  "header.h"
-// 200 
+/* 200 */  
 int numIslands(vector<vector<char>>& grid) {
-        
+    int ans = 0;
+    for(int i = 0; i < grid.size(); i++) {
+        for (int j = 0; j < grid[0].size(); j++){
+            if(grid[i][j] == '1'){
+                ans++;
+                while(true){
+                    //1转0
+                    if(grid[i][j] == '1'){
+
+                    }
+                }
+
+            }
+        }
+    }  
+    return ans;     
 }
 
 #endif
